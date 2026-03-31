d8c4f70c2925ccd73e6b0b7bc15f852d4793295c
gadearjun244 <gadearjun244@gmail.com>
2026-03-31 19:37:19 +0000

    add vercel.json

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public", "dist")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/api/users", userRouter);
@@ -54,9 +54,12 @@ app.get("/api/student/dashboard", protect, getDashboardInsights);
 app.post("/api/ai/chat", protect, chat);
 app.get("/api/ai/history", protect, getHistory);
 
-app.get(/.*/, (req, res) => {
-  return res.sendFile(path.join(__dirname, "public", "dist", "index.html"));
+
+// For any route not starting with /api, serve React
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
